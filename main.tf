fc256c07ae940bd6ce5425e5f1ffd996d03568a9
joachimuche <joachim.uche@gmail.com>
2024-05-27 14:46:33 +0100

    updates

--- main.tf
+++ main.tf
@@ -1,11 +1,17 @@
 # Data Lake Bucket
-resource "aws_s3_bucket" "cicd_core_data" {
-  bucket = "cicd-created-joachim-data-lake"
+resource "aws_s3_bucket" "terraform_cicd_core_data" {
+  bucket = "terraform-another-cicd-created-joachim-data-lake"
 
 }
 
 # Data Lake Bucket
-resource "aws_s3_bucket" "another_cicd_core_data" {
-  bucket = "another-cicd-created-joachim-data-lake"
+resource "aws_s3_bucket" "terraform_of_cicd_core_data" {
+  bucket = "terraform-cicd-created-joachim-data-lake"
+
+}
+
+# Data Lake Bucket
+resource "aws_s3_bucket" "another_terrform_cicd_core_data" {
+  bucket = "core-another-cicd-created-joachim-data-lake"
 
 }
